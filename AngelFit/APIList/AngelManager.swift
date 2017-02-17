aa37ee13f21876c083d5a1edcd3f9c4d60b39465
ganyi <283925583@qq.com>
2017-02-17 10:19:24 +0800

    closure fix

--- AngelFit/APIList/AngelManager.swift
+++ AngelFit/APIList/AngelManager.swift
@@ -97,7 +97,7 @@ public final class AngelManager: NSObject {
     }
     
     //获取设备信息
-    private func getDeviceInfoFromBand(closure : @escaping (_ errorCode:Int16 ,_ value: Any)->()){
+    private func getDeviceInfoFromBand(closure : @escaping (_ errorCode:Int16 ,_ value: Any?)->()){
         getLiveDataFromBring(withActionType: .deviceInfo, closure: closure)
     }
     
@@ -110,7 +110,7 @@ public final class AngelManager: NSObject {
     }
     
     //获取功能列表
-    private func getFuncTableFromBand(_ macAddress: String? = nil, closure : @escaping (_ errorCode:Int16 ,_ value: Any)->()){
+    private func getFuncTableFromBand(_ macAddress: String? = nil, closure : @escaping (_ errorCode:Int16 ,_ value: Any?)->()){
         getLiveDataFromBring(withActionType: .funcTable, macAddress: macAddress, closure: closure)
     }
     
@@ -144,12 +144,12 @@ public final class AngelManager: NSObject {
     }
     
     //获取实时数据
-    public func getLiveDataFromBand(closure : @escaping (_ errorCode:Int16 ,_ value: String)->()){
+    public func getLiveDataFromBand(closure : @escaping (_ errorCode:Int16 ,_ value: Any?)->()){
         getLiveDataFromBring(withActionType: .liveData, closure: closure)
     }
     
     //MARK:- 从手环端获取数据
-    private func getLiveDataFromBring(withActionType actionType:ActionType, macAddress: String? = nil, closure: @escaping (_ errorCode:Int16 ,_ value: Any) ->()){
+    private func getLiveDataFromBring(withActionType actionType:ActionType, macAddress: String? = nil, closure: @escaping (_ errorCode:Int16 ,_ value: Any?) ->()){
         
         switch actionType {
         case .macAddress:
@@ -211,7 +211,7 @@ public final class AngelManager: NSObject {
                     return
                 }
                 
-                closure(ErrorCode.success, "\(deviceInfo)")
+                closure(ErrorCode.success, deviceInfo)
             }
             
             var ret_code:UInt32 = 0
@@ -226,7 +226,7 @@ public final class AngelManager: NSObject {
             }else if let md = self.macAddress{
                 realMacAddress = md
             }else{
-                closure(ErrorCode.failure, "macAddress is empty")
+                closure(ErrorCode.failure, nil)
                 break
             }
             
@@ -339,10 +339,10 @@ public final class AngelManager: NSObject {
                 
                 debug("coreData functable: \(funcTable)")
                 guard self.coredataHandler.commit() else {
-                    closure(ErrorCode.failure, "saving failure")
+                    closure(ErrorCode.failure, nil)
                     return
                 }
-                closure(ErrorCode.success, "\(funcTable)")
+                closure(ErrorCode.success, funcTable)
             }
             debug("--------------\n开始获取funcTable")
             var ret_code:UInt32 = 0
@@ -356,7 +356,7 @@ public final class AngelManager: NSObject {
                 let liveDataStruct:protocol_start_live_data = data.assumingMemoryBound(to: protocol_start_live_data.self).pointee
                 
                 let liveData = liveDataStruct
-                closure(ErrorCode.success, "\(liveData)")
+                closure(ErrorCode.success, liveData)
             }
             var ret_code:UInt32 = 0
             vbus_tx_evt(VBUS_EVT_BASE_APP_GET, VBUS_EVT_APP_GET_LIVE_DATA, &ret_code)
