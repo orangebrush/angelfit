6bff51e6d7fe4dbaa9ae436a1c77dcf302adb43e
ganyi <283925583@qq.com>
2017-07-26 17:28:06 +0800

    0726

--- AngelFit/NetworkHandler.swift
+++ AngelFit/NetworkHandler.swift
@@ -69,4 +69,113 @@ public final class NetworkHandler {
     public class func share() -> NetworkHandler{
         return __once
     }
+    
+    //临时记录步数
+    public func updateSteps(with userId: Int, steps: Int, date: Date, closure: @escaping (_ resultCode: Int, _ message:String, _ data: Any?) -> ()){
+        do{
+            let dateStr = date.formatString(with: "yyyy-MM-dd")
+            let body: [String : Any] = [
+                "userId": userId,
+                "steps": steps,
+                "date": dateStr
+            ]
+            let requestData=try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+            
+            let url = URL(string: "http://123.207.126.246:9090/addsteps")
+            var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = requestData
+            
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: request, completionHandler: {
+                
+                data, response, error in
+                
+                //切换到主线程
+                DispatchQueue.main.async{
+                    guard error == nil else{
+                        closure(ResultCode.failure, "<记步>连接错误", nil)
+                        return
+                    }
+                    
+                    do{
+                        guard let result = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:AnyObject] else{
+                            closure(ResultCode.failure, "<记步>获取数据错误", nil)
+                            return
+                        }
+                        
+                        /*
+                         通过response["result"]判断后台返回数据是否合法
+                         result == 1 ------true
+                         result == 0 ------false
+                         */
+                        print("返回:\(result)")
+                        closure(ResultCode.success, "<记步>上传完成", result)
+                    }catch let responseError{
+                        print("response数据处理错误: \(responseError)")
+                    }
+                }
+            })
+            
+            task.resume()
+        }catch let error{
+            print("request数据处理错误: \(error)")
+        }
+    }
+    
+    //临时拉取步数
+    public func pullSteps(with target: Int, date: Date, closure: @escaping (_ resultCode: Int, _ message:String, _ data: Any?) -> ()){
+        do{
+            let dateStr = date.formatString(with: "yyyy-MM-dd")
+            let body: [String : Any] = [
+                "target": target,
+                "date": dateStr
+            ]
+            let requestData=try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+            
+            let url = URL(string: "http://123.207.126.246:9090/selectsteps")
+            var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = requestData
+            
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: request, completionHandler: {
+                
+                data, response, error in
+                
+                //切换到主线程
+                DispatchQueue.main.async{
+                    guard error == nil else{
+                        closure(ResultCode.failure, "<记步>连接错误", nil)
+                        return
+                    }
+                    
+                    do{
+                        guard let result = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String:AnyObject] else{
+                            closure(ResultCode.failure, "<记步>获取数据错误", nil)
+                            return
+                        }
+                        
+                        /*
+                         通过response["result"]判断后台返回数据是否合法
+                         result == 1 ------true
+                         result == 0 ------false
+                         */
+                        print("返回:\(result)")
+                        closure(ResultCode.success, "<记步>拉取完成", result)
+                    }catch let responseError{
+                        print("response数据处理错误: \(responseError)")
+                    }
+                }
+            })
+            
+            task.resume()
+        }catch let error{
+            print("request数据处理错误: \(error)")
+        }
+    }
 }
